751380069a21118a2b8e961d6f99b2cdd2548e85
Gaurav <cloudconfirm@gmail.com>
2022-08-22 18:22:00 +0530

    tesing commit

--- Capplan.js
+++ Capplan.js
@@ -24,7 +24,7 @@ const server_url = 'http://127.0.0.1:3000'; //localhost
 *     action: 'string',
 *     data: {
 *       // ideally contains data specific to the action string, e.g., filename, etc.
-*       filename: 'test.csv'
+*       me: 'test.csv'
 *       content: 'some-file-content'
 *     }
 *   }
